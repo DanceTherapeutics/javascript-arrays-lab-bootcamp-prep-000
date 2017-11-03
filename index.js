ccc0dccbb450c6ea56b7f28cc52bfec61bb070ed
Chris Junker <dancetherapeutics@gmail.com>
2017-11-03 15:06:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = "I don't do much."
-function destructivelyAppendKitten(name){
-  name.push("Ralph");
-  return name;
+function destructivelyAppendKitten(kittens,name){
+  kittens.push("Ralph");
+  return kittens;
 }
